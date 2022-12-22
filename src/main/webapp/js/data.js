5701a57a2b8ae339a37c0f05eba6b26cb581c75c
zsderw <oood0903@gmail.com>
2022-12-22 10:11:07 +0900

    [MODIFY] testImg

--- src/main/webapp/js/data.js
+++ src/main/webapp/js/data.js
@@ -34,7 +34,7 @@ const infoList = [
     name: '청하',
     desc: '',
     mbti: 'ISTJ',
-    img: '',
+    img: 'https://w.namu.la/s/187b05d62bb8351921860bbc3ef121fd9ba1e6c546eba5f87397d701b39eff820f6168e1e32be2125f4bc6d9682aa88d91e6b84f5a5c19a4372d68496957474415edb710b012d04ae6e37d50bbaa01ba4e918a7fe8a2c4af6b2e83bc960646352572dc5a9ef51d5ae203f6e0c270f005',
   },
   {
     name: '진로',
@@ -46,13 +46,13 @@ const infoList = [
     name: '대선',
     desc: '',
     mbti: 'ISFJ',
-    img: '',
+    img: 'https://mblogthumb-phinf.pstatic.net/MjAxOTA3MTNfMzgg/MDAxNTYzMDA4ODI1MzA4.zt5AsG91ZH1HSav8zv4zJ9rcCEStiGlbfnSv6bAE0J4g.WoHJenIeB5Q8r1eDt9iLEdJlSDR88y8wOGYRfUrehkYg.PNG.beeyaleelmk80/%EB%8C%80%EC%84%A0%EC%86%8C%EC%A3%BC_%282%29.png?type=w2',
   },
   {
     name: '자몽에 이슬',
     desc: '',
     mbti: 'ISFP',
-    img: '',
+    img: 'https://cphoto.asiae.co.kr/listimglink/7/2015062208392318794_1.jpg',
   },
   {
     name: '참이슬 오리지널',
@@ -67,28 +67,28 @@ const infoList = [
     img: '',
   },
   {
-    name: '지평생막걸리',
+    name: '장수막걸리',
     desc: '',
     mbti: 'INFJ',
-    img: '',
+    img: 'http://www.newsworker.co.kr/news/photo/202203/149584_164640_217.png',
   },
   {
-    name: '피치크러쉬',
+    name: '서울의 밤',
     desc: '',
     mbti: 'INFP',
-    img: '',
+    img: 'https://assets.business.veluga.kr/media/public/%E1%84%89%E1%85%A5%E1%84%8B%E1%85%AE%E1%86%AF%E1%84%8B%E1%85%B4_%E1%84%87%E1%85%A1%E1%86%B7.png',
   },
   {
     name: '기네스 드레프트',
     desc: '',
     mbti: 'ESTJ',
-    img: '',
+    img: 'https://www.fnnews.com/resource/media/image/2013/12/16/201312160101000411_l.jpeg',
   },
   {
     name: '화요25',
     desc: '',
     mbti: 'ESTP',
-    img: '',
+    img: 'https://cdn.veluga.kr/files/supplier/216/drinks/19429_Main1_-_HWAYO_25.png',
   },
   {
     name: '테라',
@@ -98,9 +98,9 @@ const infoList = [
   },
   {
     name: '말리부',
-    desc: '',
+    desc: '???',
     mbti: 'ESFP',
-    img: '',
+    img: 'http://www.taxtimes.co.kr/data/photos/20200731/art_15958976746839_6c3a90.jpg',
   },
   {
     name: '카스',
@@ -112,13 +112,13 @@ const infoList = [
     name: '원소주',
     desc: '',
     mbti: 'ENTP',
-    img: '',
+    img: 'https://cdn.veluga.kr/files/supplier/undefined/drinks/%E1%84%8B%E1%85%AF%E1%86%AB%20%E1%84%89%E1%85%A9%E1%84%8C%E1%85%AE(won%20soju)%20-%20%E1%84%8B%E1%85%AF%E1%86%AB%E1%84%89%E1%85%B3%E1%84%91%E1%85%B5%E1%84%85%E1%85%B5%E1%84%8E%E1%85%B3(won%20spirits).png',
   },
   {
     name: '매화수',
     desc: '',
     mbti: 'ENFJ',
-    img: '',
+    img: "https://dimg.donga.com/wps/NEWS/IMAGE/2021/02/25/105577085.5.jpg",
   },
   {
     name: '처음처럼',
--- src/main/webapp/js/start.js
+++ src/main/webapp/js/start.js
@@ -21,6 +21,15 @@ function setResult(){
   const resultName = document.querySelector('.resultname');
   resultName.innerHTML = infoList[point].name;
 
+  var resultImg = document.createElement('img');
+  const imgDiv = document.querySelector('#resultImg');
+  var imgURL = infoList[point].img;
+  console.log(infoList[point]);
+  resultImg.src = imgURL;
+  resultImg.alt = point;
+  resultImg.classList.add('img-fluid');
+  imgDiv.appendChild(resultImg);
+  
   const resultDesc = document.querySelector('.resultDesc');
   resultDesc.innerHTML = infoList[point].desc;
 }
